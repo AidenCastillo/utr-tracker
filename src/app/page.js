f4ec4583def4a3a706e58e03a4fc892afdddc70c
Aiden Castillo <76675161+AidenCastillo@users.noreply.github.com>
2023-02-04 23:18:35 -0600

    small home edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <h1 className={styles.title}>
+        Welcome to{" "}
+        <a href="https://github.com/AidenCastillo/utr-tracker">UTR Tracker</a>
+      </h1>
+      <p className={styles.description}>Uses Universal Tennis Rating to show stats, player connections, and watchlists for all your tennis needs.</p>
     </main>
-  )
+  );
 }
